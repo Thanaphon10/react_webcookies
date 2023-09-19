1e05aa9425afd4b8914e8ad5c349477402fdd9e0
Thanaphon <98265801+loseandroid@users.noreply.github.com>
2023-09-20 04:29:44 +0700

    Test add IP Address and userAgent

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,38 +1,63 @@
-import React, {useState,useEffect} from "react";
-import {collection, addDoc} from 'firebase/firestore'
+import React, { useState, useEffect } from "react";
+import { collection, addDoc } from 'firebase/firestore'
 import { db } from '../firebase'
 
-function Test (){
-    const [test,setTest] = useState("");
+function Test() {
+    const [test, setTest] = useState("");
+    const [ipAddress, setIpAddress] = useState(""); // เพิ่ม state สำหรับเก็บ IP Address
+    const [userAgent, setUserAgent] = useState(""); // เพิ่ม state สำหรับเก็บ User Agent
 
-    const addTodo = async (e) =>{
+    useEffect(() => {
+        // สร้างฟังก์ชันสำหรับเรียก IP Address
+        const getIpAddress = async () => {
+            try {
+                const response = await fetch('https://api64.ipify.org?format=json');
+                const data = await response.json();
+                const ip = data.ip;
+                setIpAddress(ip); // อัปเดต state ด้วยค่า IP Address
+            } catch (error) {
+                console.error('เกิดข้อผิดพลาดในการเรียกข้อมูล IP Address: ', error);
+            }
+        }
+
+        // เรียกฟังก์ชันเพื่อรับ IP Address เมื่อคอมโพเนนต์ถูกโหลด
+        getIpAddress();
+
+        // อ่าน User Agent และอัปเดต state
+        setUserAgent(navigator.userAgent);
+    }, []);
+
+    const addTodo = async (e) => {
         e.preventDefault();
 
         try {
-            const docRef = await addDoc(collection(db, "Test"),{
-                test: test
+            const docRef = await addDoc(collection(db, "Test"), {
+                test: test,
+                test1: test,
+                ipAddress: ipAddress, // เพิ่ม IP Address ลงในเอกสารที่เพิ่ม
+                userAgent: userAgent // เพิ่ม User Agent ลงในเอกสารที่เพิ่ม
             })
             console.log("Test ID ", docRef.id);
-        }catch(e){
+        } catch (e) {
             console.error("error adding document: ", e);
         }
     }
 
-
     return (
-
         <div>
             <input
-            type="text"
-            onChange={(e) => setTest(e.target.value)}>
+                type="text"
+                onChange={(e) => setTest(e.target.value)}>
             </input>
-            <button 
-            type="submit"
-            onClick={addTodo}>
+            <button
+                type="submit"
+                onClick={addTodo}>
                 Submit
             </button>
-        </div>
 
+            <p>IP Address: {ipAddress}</p> {/* แสดงค่า IP Address */}
+            <p>User Agent: {userAgent}</p> {/* แสดงค่า User Agent */}
+        </div>
     )
 }
 
