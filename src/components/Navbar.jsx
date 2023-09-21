46bf0482a4bdfd00a2cdcce1eb4bccb55f6ec0e9
Thanaphon <98265801+loseandroid@users.noreply.github.com>
2023-09-22 05:22:17 +0700

    เพิ่ม คุกกี้ ธีม

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,32 +5,42 @@ import { useTranslation } from "react-i18next";
 import { Button, ButtonGroup } from "react-bootstrap"; // Import Bootstrap components
 import { useCookies } from "react-cookie";
 import SetCookiesData from "./SetCookiesData";
+import Switch from "react-switch";
 
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [data, setData] = useState(false);
   const { t, i18n } = useTranslation(["home"]);
-  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const [cookies, setCookie, removeCookie] = useCookies(["user", "theme"]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    if(cookies.Name){
-      setIsLoggedIn(true)
+    if (cookies.Name) {
+      setIsLoggedIn(true);
     }
-    if(cookies.language){
+    if (cookies.language) {
       //console.log(cookies.Language)
       i18n.changeLanguage(cookies.language);
-    }else{
-      setCookie("language",i18n.language,{path: "/"})
+    } else {
+      setCookie("language", i18n.language, { path: "/" });
     }
   }, []);
 
   useEffect(() => {
     renderLoginButton();
-    if(isLoggedIn){
-    setData(true);}
+    if (isLoggedIn) {
+      setData(true);
+    }
   }, [isLoggedIn]);
 
+  useEffect(() => {
+    const themeCookie = cookies.theme;
+    if (themeCookie === "dark") {
+      setIsDarkMode(true);
+      document.body.classList.add("dark-mode");
+    }
+  }, [cookies.theme]);
 
   const handleLoginClick = () => {
     setIsModalOpen(true);
@@ -46,15 +56,14 @@ const Navbar = () => {
     setIsLoggedIn(true);
     renderLoginButton();
   };
-  console.log(cookies.Name);
 
   const renderLoginButton = () => {
     if (isLoggedIn) {
-        return (
-          <Button variant="primary" onClick={handleLogoutClick}>
-            {t("Logout")}
-          </Button>
-        );
+      return (
+        <Button variant="primary" onClick={handleLogoutClick}>
+          {t("Logout")}
+        </Button>
+      );
     } else {
       return (
         <Button variant="primary" onClick={handleLoginClick}>
@@ -64,42 +73,59 @@ const Navbar = () => {
     }
   };
 
+  const toggleDarkMode = () => {
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode);
+    setCookie("theme", newMode ? "dark" : "light", { path: "/" });
+    document.body.classList.toggle("dark-mode", newMode);
+  };
+
   return (
     <div className="navbar">
-      <div className="logo">{t("MyWebsite")}</div>
+      <div className="h3">{t("MyWebsite")}</div>
 
-      <ButtonGroup aria-label="Switch Lang">
-        <Button
-          className="btn-block btn-light"
-          onClick={() => {
-            i18n.changeLanguage("th");
-            setCookie("language",i18n.language, { path: '/' })
-          }}
-        >
-          {t("lang_th")}
-        </Button>
-        <Button
-          className="btn-block btn-light w-50"
-          onClick={() => {
-            i18n.changeLanguage("en");
-            setCookie("language",i18n.language, { path: '/' })
-          }}
-        >
-          {t("lang_en")}
-        </Button>
-      </ButtonGroup>
-
-      {renderLoginButton()}
-
+      <div className={`app ${isDarkMode ? "dark" : "light"}`}>
+        <main>
+          <label>
+            เปิดโหมดธีมดำ
+            <Switch onChange={toggleDarkMode} checked={isDarkMode} />
+            <p>{cookies.theme}</p>
+          </label>
+        </main>
+      </div>
+      <div>
+        <ButtonGroup aria-label="Switch Lang">
+          <Button
+            className="btn-block btn-light"
+            onClick={() => {
+              i18n.changeLanguage("th");
+              setCookie("language", i18n.language, { path: "/" });
+            }}
+            disabled={i18n.language === "th"}
+          >
+            {t("lang_th")}
+          </Button>
+          <Button
+            className="btn-block btn-light"
+            onClick={() => {
+              i18n.changeLanguage("en");
+              setCookie("language", i18n.language, { path: "/" });
+            }}
+            disabled={i18n.language === "en"}
+          >
+            {t("lang_en")}
+          </Button>
+        </ButtonGroup>
+      </div>
+      <div>{renderLoginButton()}</div>
       {isModalOpen && (
         <LoginModal
           setIsModalOpen={setIsModalOpen}
           handleLoginSuccess={handleLoginSuccess}
         />
       )}
-      {data &&
-      <SetCookiesData open ={isLoggedIn}/>
-      }
+      <br/>
+      {data && <SetCookiesData open={isLoggedIn} />}
     </div>
   );
 };
